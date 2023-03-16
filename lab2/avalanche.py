e5ca6b260cd7c4703fca0aff240b343d041a4346
ahatem20 <a.hatem20@live.com>
2023-03-16 11:02:03 +0200

    update

--- lab2/avalanche.py
+++ lab2/avalanche.py
@@ -4,10 +4,11 @@ Modes of Operation: Image Encryption
 
 import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-key = os.urandom(32)
+key = os.urandom(16)
 
 # Plaintext data (Hex)
 data = bytes.fromhex('100102030405060708090A0B0C0D0E0F')
+print('Plaintext: ', data.hex())
 
 # Initialize Cipher with key and mode.
 cipher = Cipher(algorithms.AES(key), modes.ECB())
